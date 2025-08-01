fa2e37254b8521b72fd6bbfdfab89676dc6a57e2
Marcos Kano <71046148+xbvkano@users.noreply.github.com>
2025-07-31 19:50:07 -0700

    make nav sticky on desktop

--- client/src/Admin/AdminDashboard.tsx
+++ client/src/Admin/AdminDashboard.tsx
@@ -21,7 +21,7 @@ export default function AdminDashboard({ onLogout }: Props) {
 
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900">
-      <nav className="bg-white shadow md:mb-4 fixed bottom-0 w-full md:static z-10">
+      <nav className="bg-white shadow md:mb-4 fixed bottom-0 w-full md:sticky md:top-0 z-10">
         <ul className="flex flex-wrap justify-around p-2 text-sm">
           <li><Link className="px-2 py-1" to="/dashboard">Home</Link></li>
           <li><Link className="px-2 py-1" to="/dashboard/calendar">Calendar</Link></li>
