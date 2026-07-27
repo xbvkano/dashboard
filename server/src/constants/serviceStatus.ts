5f0d56e3383935d8ae8c1d6cc09100da2f601060
Marcos Kano <kanomarcos1974@gmail.com>
2026-07-27 10:39:09 -0700

    Fixed 30 minute text message

--- server/src/constants/serviceStatus.ts
+++ server/src/constants/serviceStatus.ts
@@ -8,7 +8,7 @@ export const SERVICE_STATUS_SMS_BODIES: Partial<Record<ServiceStatusKind, string
   ON_THE_WAY:
     'Hi! This is Evidence Cleaning. Your cleaning team is on the way to your appointment.',
   THIRTY_MINUTES_LEFT:
-    'The cleaning team just informed us that they will be done with the cleaning in about minutes. Please let me know if they did a good job 😊.',
+    'The cleaning team just informed us that they will be done with the cleaning in about 30 minutes. Please let me know if they did a good job 😊. If you’d like to schedule your next cleaning now, just let me know! Due to high demand, this helps us secure the best date and time for you.',
 }
 
 export function isServiceStatusKind(value: string): value is ServiceStatusKind {
